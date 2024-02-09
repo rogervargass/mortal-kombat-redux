9bc3636012252d04bcca68f824f4a233d8c08a97
Roger Vargas <rogerfsvargas@hotmail.com>
2024-02-09 15:26:10 -0300

    feat: add music theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import ReactAudioPlayer from "react-audio-player";
 import { Provider } from "react-redux";
+import MK_THEME from "./assets/audio/theme_music.mp3";
 import Router from "./routes/Router";
 import { store } from "./store/store";
 
@@ -6,6 +8,7 @@ function App() {
   return (
     <Provider store={store}>
       <Router />
+      <ReactAudioPlayer src={MK_THEME} autoPlay />
     </Provider>
   );
 }
